2db062c41d74f5f76198ecd050fa34e92c9407b4
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-03-19 13:28:14 -0600

    wrote code to display message based on the time of day

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ var time = new Date().getHours();
 var messageText;
 var wakeupTime = 7;
 var noon = 12;
-var lunchTime = 13;
-var napTime = 15;
-var evening = 17;
-var partyTime = 22;
+var lunchTime = 13; // 1 pm
+var napTime = 15; // 3 pm
+var evening = 17; // 5 pm
+var partyTime = 22; // 10 pm
+var message = document.getElementById('timeEvent');
 
 if (time == wakeupTime) {
 	messageText = "It's wake up time!";
@@ -21,8 +22,7 @@ if (time == wakeupTime) {
     messageText = "It's party time, baller!";
 }
 
-console.log(messageText);
-
+message.innerText = messageText;
 
 
 
